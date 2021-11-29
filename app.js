fa6e83938931b6876654e508b754f887ce35df94
Manu Choudhary <manu.choudhary@grazitti.local>
2021-11-29 14:17:49 +0530

    changed to enter event

--- app.js
+++ app.js
@@ -11,26 +11,29 @@ const ui = new UI();
 let searchUser = document.getElementById("searchUser");
 
 searchUser.addEventListener("keyup", (e) => {
-  //Get input Text
-  let userText = e.target.value;
+  if (e.key == "Enter") {
+    e.preventDefault();
+    //Get input Text
+    let userText = e.target.value;
 
-  if (userText !== "") {
-    //make Http call
+    if (userText !== "") {
+      //make Http call
 
-    github.getUser(userText).then((data) => {
-      if (data.profile.message === "Not Found") {
-        //Show Alert
-        ui.showAlert("User Not Found", "alert alert-danger");
-      } else {
-        // Show profile data
-        ui.showProfile(data.profile);
-        ui.showRepos(data.repos);
-      }
-    });
-  } else {
-    //Clear profile
-    ui.clearProfile();
-    //Clear Repos
-    ui.clearRepos();
+      github.getUser(userText).then((data) => {
+        if (data.profile.message === "Not Found") {
+          //Show Alert
+          ui.showAlert("User Not Found", "alert alert-danger");
+        } else {
+          // Show profile data
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      });
+    } else {
+      //Clear profile
+      ui.clearProfile();
+      //Clear Repos
+      ui.clearRepos();
+    }
   }
 });
